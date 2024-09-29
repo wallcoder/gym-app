f0f08755d568dfb6cbea34dfd1b3dba58f4b50eb
cen1c <sbfactual@gmail.com>
2024-09-29 20:43:01 +0530

    replaced the web folder with another web folder having admin template

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,20 @@ import express from "express";
 import path from 'path';
 import db from './config/db.js'
 
+import { Gym } from "./models/Gym.js";
+
 const app = express();
 
 
 
 app.listen(3000, ()=>console.log("Listening at port 3000"))
 
-app.get('/', (req, res)=>{
-    res.send("hellloooo")
+app.get('/', async (req, res)=>{
+    try{
+        const gyms = await Gym.find({})
+        res.json(gyms)
+    }catch(err){
+        res.send(err)
+    }
+    
 })
